188ead9f4c1f888827703dc1f7e4c206dcc3b446
Kriti Rai <kriti.rai@gmail.com>
2018-04-19 00:43:44 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 14, M, "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 20, M, "Rowdy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 30, F, "Aggressive", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("sergeant Brown", 5, M, "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 29, F, "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 45, M, "Calm", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 25, F, "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 28, M, NULL, 0);
-
-
+INSERT INTO bears VALUES ("Mr. Chocolate", 14, "M", "Calm", 1);
+INSERT INTO bears VALUES ("Rowdy", 20, "M", "Rowdy", 1);
+INSERT INTO bears VALUES ("Tabitha", 30, "F", "Aggressive", 0);
+INSERT INTO bears VALUES ("sergeant Brown", 5, "M", "Calm", 1);
+INSERT INTO bears VALUES ("Melissa", 29, "F", "Calm", 1);
+INSERT INTO bears VALUES ("Grinch", 45, "M", "Calm", 0);
+INSERT INTO bears VALUES ("Wendy", 25, "F", "Calm", 1);
+INSERT INTO bears VALUES (NULL, 28, "M", NULL, 0);
